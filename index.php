91b7a4ab518f5f3069bd3c8c292e85e1ddb1a9f7
crike2000 <78335470+crike2000@users.noreply.github.com>
2021-02-01 12:42:09 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UFT-8">
+  <meta name="veiwport"content="witdh=device-width, initial-scale=1.0">
+  <title>Document</title>
+ </head>
+ <body>
+    <p>Welcome to my page ERICK S. SUSTITUIDO</p>
+ </body>
+ </html>
